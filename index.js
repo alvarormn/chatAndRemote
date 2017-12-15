95eaab8550c0adeb0ed309e2898cc535b54e1107
alvarormn <alvarorodriguezmn@gmail.com>
2017-12-15 12:00:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  //var url = __dirname + '/index.html'
+  var url = '<!doctype html>' +
+  '<html>' +
+    '<head>' +
+      '<title>Socket.IO chat</title>' +
+      '<style>' +
+        '* { margin: 0; padding: 0; box-sizing: border-box; }' +
+        'body { font: 13px Helvetica, Arial; }' +
+        'form { background: #000; padding: 3px; position: fixed; bottom: 0; width: 100%; }' +
+        'form input { border: 0; padding: 10px; width: 90%; margin-right: .5%; }' +
+        'form button { width: 9%; background: rgb(130, 224, 255); border: none; padding: 10px; }' +
+        '#messages { list-style-type: none; margin: 0; padding: 0; }' +
+        '#messages li { padding: 5px 10px; }' +
+        '#messages li:nth-child(odd) { background: #eee; }' +
+      '</style>' +
+    '</head>' +
+    '<body>' +
+      '<ul id="messages"></ul>' +
+      '<form>' +
+        '<input id="m" autocomplete="off" /><button>Send</button>' +
+      '</form>' +
+    '</body>' +
+    '<script src="/socket.io/socket.io.js"></script>' +
+    '<script src="https://code.jquery.com/jquery-1.11.1.js"></script>' +
+    '<script>' +
+      '$(function () {' +
+        'var socket = io();' +
+        '$(\'form\').submit(function(){' +
+          'socket.emit(\'chat message\', $(\'#m\').val());' +
+          '$(\'#m\').val(\'\');' +
+          'return false;' +
+        '});' +
+        'socket.on(\'chat message\', function(msg){' +
+          '$(\'#messages\').append($(\'<li>\').text(msg));' +
+        '});' +
+      '});' +
+    '</script>' +
+  '</html>'
+  res.send(url);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+io.emit('some event', { for: 'everyone' });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
